3dd726f84a17b5c961d0604d96d71973a4f80f65
Chris Barrett <chris.d.barrett@me.com>
2017-01-04 09:32:45 +1300

    Get stylus working

--- reduxstagram/config/webpack.config.prod.js
+++ reduxstagram/config/webpack.config.prod.js
@@ -87,7 +87,7 @@ module.exports = {
       'react-native': 'react-native-web'
     }
   },
-  
+
   module: {
     // First, run the linter.
     // It's important to do this before Babel processes the JS.
@@ -116,6 +116,7 @@ module.exports = {
           /\.html$/,
           /\.(js|jsx)$/,
           /\.css$/,
+          /\.styl/,
           /\.json$/,
           /\.svg$/
         ],
@@ -130,8 +131,16 @@ module.exports = {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
         loader: 'babel',
-        
+
       },
+
+      // stylus loader
+      {
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1!postcss!stylus')
+        // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
+      },
+
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
       // "css" loader resolves paths in CSS and adds assets as dependencies.
@@ -165,7 +174,7 @@ module.exports = {
       }
     ]
   },
-  
+
   // We use PostCSS for autoprefixing only.
   postcss: function() {
     return [
--- reduxstagram/src/index.js
+++ reduxstagram/src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/Main';
 
+import css from './styles/style.styl'; // eslint-disable-line no-unused-vars
+
 ReactDOM.render(
   <Main />,
   document.getElementById('root')
